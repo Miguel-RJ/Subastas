b69bef2ad2b895352f8d1027e1998b49ebf7bf0f
Miguel Ramírez <mramirez_juarez@outlook.com>
2021-12-05 01:22:46 -0600

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Subastas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
